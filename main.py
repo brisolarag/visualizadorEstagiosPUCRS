a359bf3ed0b4693d34162f060d50ef0687ee922a
Gabriel Brisolara <brisolara@gabriel-mac.local>
2023-09-16 09:40:07 -0300

    🫡 melhoria na visualizacao e organizacao

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 # chrome_driver_path = "./chromedriver"
 url = "https://carreiras.pucrs.br/Oportunidades"
 
 navegador = webdriver.Chrome()
-
 navegador.get(url)
 
 # Espera pagina carregar
-wait = WebDriverWait(navegador, 20)
-wait.until(EC.presence_of_element_located((By.ID, "CursoNome")))
+sleep(15)
 
+# ======= SELECIONANDO CURSO E ENVIANDO
+
+# Seleciona curso engenharia mecânica
 curso_select = Select(navegador.find_element(By.ID, "CursoNome"))
-
 curso_select.select_by_value("engenharia-de-software") #seleciona curso
 
-
+sleep(1) # checa se o curso nao esta escolhido
 if curso_select.first_selected_option.get_attribute("value") != "engenharia-de-software":
     curso_select.select_by_value("engenharia-de-software")
 
-wait2 = WebDriverWait(navegador, 10)
-wait2.until(EC.presence_of_element_located((By.CLASS_NAME, "vagas-enviar")))
-
+# envia
 botao_enviar = navegador.find_element(By.CLASS_NAME, "vagas-enviar")
 botao_enviar.click()
 
-# Espera pagina carregar
-wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".oportunidade-item h3")))
-wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".oportunidade-item p")))
 
+# espera pagina carregar
+sleep(10)
+
+# ======== PEGANDO AS VAGAS
+
+a_elements = navegador.find_elements(By.XPATH, "//*[@id='lista']/a")
 h3_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item h3")
 p_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item p")
 
-# Espera pagina carregar
 
-vagas = [h3.text for h3 in h3_elements]
 
-for vaga in vagas:
-    print(vaga)
+
+_vagas = [h3.text for h3 in h3_elements]
+_descricao = [p.text for p in p_elements]
+_hrefs = [a.get_attribute('href') for a in a_elements]
+inicioURL = "https://carreiras.pucrs.br"
+vagas = []
+
+for i, vaga in enumerate(_vagas):
+    dict = {'indice': i, 'vaga': vaga, 'descricao': _descricao[i], 'link': inicioURL + _hrefs[i] }
+    vagas.append(dict)
+
+print(f'Foram encontradas {len(_vagas)} vagas.')
+
+for i, vaga in enumerate(vagas):
+    print(f'Vaga {i + 1} de {len(_vagas)}:')
+    print(f'Vaga: {vaga["vaga"]}')
+    print(f'Descricao: {vaga["descricao"]}')
+    print(f'Link: {vaga["link"]}')
+    
+
 
 navegador.quit()
