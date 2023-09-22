a6b083c43647a83d12890f95412878c00891bccb
Gabriel Brisolara <brisolara@gabriel-mac.local>
2023-09-22 16:06:49 -0300

    🥸 agora adiciona os estagios em documentos txt dentro de 'vagas'

--- main.py
+++ main.py
@@ -4,60 +4,64 @@ from selenium.webdriver.support.ui import Select
 from time import sleep
 import write_file
 
-# chrome_driver_path = "./chromedriver"
+
+################################ NAVEGANDO ATÉ A PÁGINA #################################
 url = "https://carreiras.pucrs.br/Oportunidades"
 
 navegador = webdriver.Chrome()
 navegador.get(url)
 
 # Espera pagina carregar
-sleep(15)
+sleep(10)
 
-# ======= SELECIONANDO CURSO E ENVIANDO
 
+################################ SELECIONANDO CURSO E ENVIANDO #################################
 # Seleciona curso engenharia mecânica
 curso_select = Select(navegador.find_element(By.ID, "CursoNome"))
 curso_select.select_by_value("engenharia-de-software") #seleciona curso
 
-sleep(1) # checa se o curso nao esta escolhido
-if curso_select.first_selected_option.get_attribute("value") != "engenharia-de-software":
-    curso_select.select_by_value("engenharia-de-software")
+# sleep(1)
 
-# envia
+# Checa se o curso esta realmente escolhido
+# if curso_select.first_selected_option.get_attribute("value") != "engenharia-de-software":
+#     curso_select.select_by_value("engenharia-de-software")
+
+# Scrolla para botao ficar clicavel e depois envia
+navegador.execute_script("window.scrollBy(0, 80);")
 botao_enviar = navegador.find_element(By.CLASS_NAME, "vagas-enviar")
 botao_enviar.click()
 
 
-# espera pagina carregar
-sleep(10)
-
-# ======== PEGANDO AS VAGAS
-
-a_elements = navegador.find_elements(By.XPATH, "//*[@id='lista']/a")
-h3_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item h3")
-p_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item p")
+# Espera próxima pagina carregar
+sleep(5)
 
 
+################################ ITERANDO SOBRE AS VAGAS #################################
+h3_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item h3") #   titulo das vagas            / elemento h3
+p_elements = navegador.find_elements(By.CSS_SELECTOR, ".oportunidade-item p") #     descricao das vagas         / elemento p
+a_elements = navegador.find_elements(By.XPATH, "//*[@id='lista']/a") #              link das vagas              / elemento a
 
 
-_vagas = [h3.text for h3 in h3_elements]
-_descricao = [p.text for p in p_elements]
-_hrefs = [a.get_attribute('href') for a in a_elements]
+
+
+_vagas = [h3.text for h3 in h3_elements] #                  pega todas as vagas
+_descricao = [p.text for p in p_elements] #                 pega todas as descricoes
+_hrefs = [a.get_attribute('href') for a in a_elements] #    pega todos links
+
+
 vagas = []
 
+################################ ATRIBUINDO AS VAGAS AO ARRAY #################################
 for i, vaga in enumerate(_vagas):
-    dict = {'indice': i, 'vaga': vaga, 'descricao': _descricao[i], 'link': _hrefs[i] }
+    dict = {'indice': i, 'vaga': write_file.substituir_barras(vaga), 'descricao': _descricao[i], 'link': _hrefs[i] }
     vagas.append(dict)
 
 print(f'Foram encontradas {len(_vagas)} vagas.')
 
+
+################################ ESCREVENDO OS TXT DAS VAGAS #################################
 for vaga in vagas:
     write_file.escrever_arquivo(vaga)
-    # print(f'Vaga {i + 1} de {len(_vagas)}:')
-    # print(f'Vaga: {vaga["vaga"]}')
-    # print(f'Descricao: {vaga["descricao"]}')
-    # print(f'Link: {vaga["link"]}')
-    # print("")
     
 
 
--- write_file.py
+++ write_file.py
@@ -1,33 +1,30 @@
 from datetime import datetime
+import os
 hoje = datetime.now().strftime('%d_%m_%Y')
 
-def escrever_arquivo(arr):
-    _i = 1
-    try:
-        f = open(f'vagas - {_i}.txt')
-        for vaga in arr:
-            linha0 = (f'Vaga {_i}:')
-            linha1 = (f'Vaga: {vaga["vaga"]}')
-            linha2 = (f'Descricao: {vaga["descricao"]}')
-            linha3 = (f'Link: {vaga["link"]}')
+def escrever_arquivo(dict):
+    try:    
+        file_name = f'{str(dict["indice"]).zfill(3)}.{dict["vaga"]}.txt'
+        file_location = os.path.join('vagas/', file_name)
 
-            f.write(f"{linha0}\n{linha1}\n{linha2}\n{linha3}\n")
 
-    except Exception as e:
-        while(e):
-            _i += 1
+        f = open(file_location, 'x')
         
-        f = open(f'vagas - {_i}.txt')
-        for vaga in arr:
-            linha0 = (f'Vaga {_i}:')
-            linha1 = (f'Vaga: {vaga["vaga"]}')
-            linha2 = (f'Descricao: {vaga["descricao"]}')
-            linha3 = (f'Link: {vaga["link"]}')
+        linha0 = (f"Vaga {dict['indice']}:")
+        linha1 = (f"Vaga: {dict['vaga']}")
+        linha2 = (f"Descricao: {dict['descricao']}")
+        linha3 = (f"Link: {dict['link']}")
 
-            f.write(f"{linha0}\n{linha1}\n{linha2}\n{linha3}\n")
-
+        f.write(f"{linha0}\n{linha1}\n{linha2}\n{linha3}\n")
+    except FileExistsError as fe:
+        print(f'Ocorreu um erro: <{file_name}> já existe.')
 
 
 if __name__ == "__main__":
     print("not in main")
+    dict_teste = {'indice': 2, 'vaga': 'ESTÁGIO - APRIX - FULL STACK DEVELOPER', 'descricao': 'A Aprix é uma startup que desenvolve soluções de ciência de dados, principalmente voltadas para precificação. Somos focados no desenvolvimento de soluções de pricing baseadas em Inteligência Artificial, com case de repercussão nacional no setor de combustíveis. Nossa solução de precificação dinâmica e otimização de preços para postos de combustíveis é pioneira no mercado nacional e está em utilização no país inteiro. Mas é só o começo. Vamos alterar a forma como preços são formulados e executados em diferentes segmentos da indústria e varejo nos próximos anos. Nossa missão é construir soluções de Data Science que auxiliem agentes de diferentes portes e setores a alcançar ganhos de eficiência.', 'link': 'https://carreiras.pucrs.br/Oportunidades/estagio/26401/estagio---aprix---full-stack-developer'}
+    escrever_arquivo(dict_teste)
     
+
+def substituir_barras(string):
+    return string.replace('/', '_')
